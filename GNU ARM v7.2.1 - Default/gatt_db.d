26486a6168decd42fbf1460830258b18004990fb
Brian <bdrr77@gmail.com>
2019-12-27 15:23:27 +0100

    Cleanup

--- GNU ARM v7.2.1 - Default/gatt_db.d
+++
@@ -1,7 +0,0 @@
-gatt_db.o: ../gatt_db.c \
- C:\Users\Brian\SimplicityStudio\v4_workspace_Clean\soc-empty-gpd-dmp\protocol\bluetooth\ble_stack\inc\common/bg_gattdb_def.h \
- C:\Users\Brian\SimplicityStudio\v4_workspace_Clean\soc-empty-gpd-dmp\protocol\bluetooth\ble_stack\inc\common/bg_types.h
-
-C:\Users\Brian\SimplicityStudio\v4_workspace_Clean\soc-empty-gpd-dmp\protocol\bluetooth\ble_stack\inc\common/bg_gattdb_def.h:
-
-C:\Users\Brian\SimplicityStudio\v4_workspace_Clean\soc-empty-gpd-dmp\protocol\bluetooth\ble_stack\inc\common/bg_types.h:
--- GNU ARM v7.2.1 - Default/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS := C:/Users/Brian/SimplicityStudio/v4_workspace_Clean/soc-empty-gpd-dmp/protocol/bluetooth/lib/EFR32MG12P/GCC/binapploader.o C:/Users/Brian/SimplicityStudio/v4_workspace_Clean/soc-empty-gpd-dmp/protocol/bluetooth/lib/EFR32MG12P/GCC/libbluetooth.a C:/Users/Brian/SimplicityStudio/v4_workspace_Clean/soc-empty-gpd-dmp/platform/radio/rail_lib/autogen/librail_release/librail_multiprotocol_efr32xg12_gcc_release.a C:/Users/Brian/SimplicityStudio/v4_workspace_Clean/soc-empty-gpd-dmp/protocol/bluetooth/lib/EFR32MG12P/GCC/libpsstore.a
-
-LIBS := -lm
-
